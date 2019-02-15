3207403d4194918e16ac3dd0076c7702de906e4c
dylan-lom <39334387+dylan-lom@users.noreply.github.com>
2019-02-15 11:36:23 +1100

    Create stupid.sol

---
+++ stupid.sol
@@ -0,0 +1,24 @@
+pragma solidity ^0.4.19;
+
+contract blah {
+    bytes32 DateString = "150219";
+    uint datePos;
+    uint monthPos;
+    bytes32 Days;
+    bytes32 Month;
+    
+    
+    function getIndex(uint index) public view returns (bytes1) {
+        return DateString[index];
+    }
+    
+    function getDatePos(uint pos) public {
+        uint datePos = pos;
+    }
+    function getMonthPos(uint pos) public {
+        uint monthPos = pos;
+    }
+    
+    
+    for (uint)
+}
